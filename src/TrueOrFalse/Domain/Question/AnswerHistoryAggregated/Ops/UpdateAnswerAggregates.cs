06f78622f4c2a962c3562d1e9603324ca08b3266
Robert Mischke <robertmischke@gmail.com>
2018-07-28 00:44:31 +0200

    answer aggregate updates

--- src/TrueOrFalse/Domain/Question/AnswerHistoryAggregated/Ops/UpdateAnswerAggregates.cs
+++ src/TrueOrFalse/Domain/Question/AnswerHistoryAggregated/Ops/UpdateAnswerAggregates.cs
@@ -1,41 +1,35 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 
 public class UpdateAnswerAggregates
 {
-    /// <summary>
-    /// Considers all answers ever given
-    /// </summary>
-    public static void FullUpdate()
+    /// <summary>Updates answer aggregates for the given user</summary>
+    public static void FullUpdate(User user) => _FullUpdate(false, user);
+
+    /// <summary>Updates answer aggregates for users</summary>
+    public static void FullUpdate(bool forUsersWhoLoggedInAfterPreviousFullUpdate = true) => _FullUpdate(forUsersWhoLoggedInAfterPreviousFullUpdate);
+
+    private static void _FullUpdate(bool forUsersWhoLoggedInAfterPreviousFullUpdate = true, User userToUpdate = null)
     {
         Logg.r().Information("UpdateAnswerAggregates");
 
-        var users = Sl.UserRepo.GetAll();
+        var users = userToUpdate == null ? 
+            GetUsers(forUsersWhoLoggedInAfterPreviousFullUpdate) :
+            new List<User> { userToUpdate };
+
         var answerAggregatedRepo = Sl.AnswerAggregatedRepo;
 
         var allAggregatedEntries = answerAggregatedRepo.GetAll();
-
-        var historyEntry = Sl.JobHistoryRepo.GetLastUpdateAnswerAggregates();
-
-        if (historyEntry != null)
-        {
-            users = users
-                .Where(user => user.LastLogin != null && user.LastLogin > historyEntry.FinishedAt)
-                .ToList();
-        }
 
         foreach (var user in users)
         {
             Logg.r().Information("UpdateAnswerAggregates: Start user {0}", user.Id);
 
-            var allAnswersByQuestion = Sl.AnswerRepo
-                .GetByUser(user.Id)
-                .GroupBy(answer => answer.Question.Id);
+            var allAnsweredQuestionIds = Get_all_answered_question_ids(user);
 
-            foreach (var answersByQuestion in allAnswersByQuestion)
+            foreach (var questionId in allAnsweredQuestionIds)
             {
-                var questionId = answersByQuestion.Key;
-
                 var entryByQuestionAndUserId = 
                     allAggregatedEntries.FirstOrDefault(x => x.QuestionId == questionId && x.UserId == user.Id);
 
@@ -64,5 +58,38 @@ public class UpdateAnswerAggregates
                 }
             }
         }
+    }
+
+    private static IEnumerable<int> Get_all_answered_question_ids(User user)
+    {
+        //potential improvement: consider answer date
+
+        return Sl.AnswerRepo
+            .GetByUser(user.Id)
+            .GroupBy(answer => answer.Question.Id)
+            .Select(answersByQuestion => answersByQuestion.Key);
+    }
+
+    private static IList<User> GetUsers(bool forUsersWhoLoggedInAfterPreviousFullUpdate)
+    {
+        if (forUsersWhoLoggedInAfterPreviousFullUpdate)
+            return Get_all_users_who_logged_in_after_the_last_full_update();
+
+        return Sl.UserRepo.GetAll();
+    }
+
+    private static IList<User> Get_all_users_who_logged_in_after_the_last_full_update()
+    {
+        var users = Sl.UserRepo.GetAll();
+
+        var historyEntry = Sl.JobHistoryRepo.GetLastUpdateAnswerAggregates();
+        if (historyEntry != null)
+        {
+            users = users
+                .Where(user => user.LastLogin != null && user.LastLogin > historyEntry.FinishedAt)
+                .ToList();
+        }
+
+        return users;
     }
 }
--- src/TrueOrFalse/Tools/ScheduledJobs/JobHistory/JobHistory.cs
+++ src/TrueOrFalse/Tools/ScheduledJobs/JobHistory/JobHistory.cs
@@ -3,7 +3,7 @@ using Seedworks.Lib.Persistence;
 
 public class JobHistory : IPersistable
 {
-    public int Id { get; set; }
+    public virtual int Id { get; set; }
     public virtual JobHistoryType Type { get; set; }
 
     public virtual DateTime FinishedAt { get; set; }
