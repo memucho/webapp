ce402c22f252108fba2037bdf8115f378693b49d
Dandor <daniel.majunke@gmx.de>
2019-04-29 22:08:18 +0200

    beetwenn raw

---
+++ src/TrueOrFalse.Tests/5 Utiltities/ReputationsPointsAsTextTests.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+
+    class ReputationsPointsAsTextTests : BaseTest
+    {
+        [Test]
+        public void Should_format_correctly()
+        {
+            Assert.That(ReputationsPointsAsText.Run(0),Is.EqualTo("0 Punkte"));
+            Assert.That(ReputationsPointsAsText.Run(999),Is.EqualTo("999 Punkte"));
+            Assert.That(ReputationsPointsAsText.Run(1000),Is.EqualTo("1 Tsd. Punkte"));
+            Assert.That(ReputationsPointsAsText.Run(1001),Is.EqualTo("1 Tsd. Punkte"));
+
+        }
+    }
+
---
+++ src/TrueOrFalse/Infrastructure/Reputation/ReputationsPointsAsText.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+ public class ReputationsPointsAsText{
+
+        public static string Run(int repoPoints)
+        {
+
+
+            return "";
+        }
+ }
+
