e5845bfbc0ce6c86587490fc1e747b67297f85b1
Daniel-Majunke <daniel.majunke@gmx.de>
2021-07-15 14:02:05 +0200

    add Column PaymentStatus to table  user

---
+++ src/TrueOrFalse/Tools/Update/Steps/UpdateToVs226.cs
@@ -0,0 +1,16 @@
+﻿using NHibernate;
+
+namespace TrueOrFalse.Updates
+{
+    class UpdateToVs226
+    {
+        public static void Run()
+        {
+            Sl.Resolve<ISession>()
+                .CreateSQLQuery(
+                    @"ALTER TABLE `user`
+	                ADD COLUMN `PaymentStatus` TINYINT  NOT NULL DEFAULT 0 AFTER `LearningSessionOptions`;"
+                ).ExecuteUpdate();
+        }
+    }
+}
